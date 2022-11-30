d34c95c098c75b629f95aab822b2d19eabb535c0
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-11-30 16:45:13 +0300

    DAY02 ex08 fix

--- DAY02/src/ex08/day02_ex08.sql
+++ DAY02/src/ex08/day02_ex08.sql
@@ -4,3 +4,4 @@ LEFT JOIN person_order ON person.id = person_order.person_id
 LEFT JOIN menu ON menu.id = person_order.menu_id
 WHERE person.gender = 'male' AND (person.address = 'Moscow' or person.address = 'Samara')
 AND (menu.pizza_name = 'mushroom pizza' OR menu.pizza_name = 'pepperoni pizza')
+ORDER BY person.name DESC;
