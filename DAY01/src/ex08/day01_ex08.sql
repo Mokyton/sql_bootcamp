ab61e111543c38da073301ab4cb885a3e8a126b1
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-11-30 11:17:20 +0300

    DAY01  micro fix

--- DAY01/src/ex08/day01_ex08.sql
+++ DAY01/src/ex08/day01_ex08.sql
@@ -1,6 +1,6 @@
 SELECT order_date,
        format('%s (age:%s)', person.name, person.age) as person_information
-FROM person NATURAL  JOIN (SELECT id as order_id, order_date, person_id as id FROM person_order) as pv
+FROM person NATURAL  JOIN (SELECT order_date, person_id as id FROM person_order) as pcdc
 ORDER BY order_date ASC,
          person_information ASC;
 
