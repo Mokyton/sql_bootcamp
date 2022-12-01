5e7e6b464e435f6ab88fcb1458f585e4891f902c
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-01 13:08:26 +0300

    DAY03 ex03 done

---
+++ DAY03/src/ex03/day03_ex03.sql
@@ -0,0 +1,23 @@
+WITH female As (
+    SELECT pizzeria.name
+    FROM person
+    LEFT JOIN person_visits ON person.id = person_visits.person_id
+    LEFT JOIN pizzeria ON pizzeria.id = person_visits.pizzeria_id
+    WHERE person.gender = 'female' AND person.gender != 'male'
+),
+     male AS (
+    SELECT pizzeria.name
+    FROM person
+    LEFT JOIN person_visits ON person.id = person_visits.person_id
+    LEFT JOIN pizzeria ON pizzeria.id = person_visits.pizzeria_id
+    WHERE person.gender = 'male' AND person.gender != 'female'
+     ),
+     femaleOnly AS (
+         SELECT * FROM female EXCEPT ALL SELECT * FROM male
+     ),
+     maleOnly AS (
+         SELECT * FROM male EXCEPT ALL SELECT * from female
+     )
+SELECT name pizzeria_name
+FROM femaleOnly UNION ALL SELECT name FROM maleOnly
+ORDER BY pizzeria_name;
