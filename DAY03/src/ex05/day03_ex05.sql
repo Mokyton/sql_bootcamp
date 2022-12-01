b325764812025cc6cd3b7e574f4babb55782acf8
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-01 14:27:43 +0300

    fix

--- DAY03/src/ex05/day03_ex05.sql
+++ DAY03/src/ex05/day03_ex05.sql
@@ -1,6 +1,24 @@
-SELECT p.name as pizzeria_name
-FROM person
-JOIN person_order po on person.id = po.person_id
-JOIN person_visits pv on person.id = pv.person_id
-JOIN pizzeria p on pv.pizzeria_id = p.id
-WHERE person.name = 'Andrey' and po.order_date != pv.visit_date
+WITH visit As (
+    SELECT p.name
+    FROM person
+    LEFT JOIN person_visits pv on person.id = pv.person_id
+    LEFT JOIN pizzeria p on pv.pizzeria_id = p.id
+    WHERE person.name = 'Andrey'
+),
+     ordered AS (
+    SELECT pizzeria.name
+    FROM person
+    LEFT JOIN person_order ON person.id = person_order.person_id
+    LEFT JOIN menu ON person_order.menu_id = menu.id
+    LEFT JOIN pizzeria ON menu.pizzeria_id = pizzeria.id
+    WHERE person.name = 'Andrey'
+     ),
+     uk AS (
+         SELECT * FROM visit EXCEPT SELECT * FROM ordered
+     ),
+     qk AS (
+         SELECT * FROM ordered EXCEPT SELECT * from visit
+     )
+SELECT name pizzeria_name
+FROM uk UNION  SELECT name FROM qk
+ORDER BY pizzeria_name;
