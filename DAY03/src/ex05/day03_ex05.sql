9bc5ca71bcacecbe42e13435a4a1ebf83b3ea109
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-01 13:53:18 +0300

    DAY03 ex05 done

--- DAY03/src/ex05/day03_ex05.sql
+++ DAY03/src/ex05/day03_ex05.sql
@@ -1,3 +1,6 @@
-WITH tmp AS (
-
-)
+SELECT p.name as pizzeria_name
+FROM person
+JOIN person_order po on person.id = po.person_id
+JOIN person_visits pv on person.id = pv.person_id
+JOIN pizzeria p on pv.pizzeria_id = p.id
+WHERE person.name = 'Andrey' and po.order_date != pv.visit_date
