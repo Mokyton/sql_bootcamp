2a60f16cba946da6ec9670b3e983f565d201af98
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-02 13:29:35 +0300

    DAY04 ex04 done

---
+++ DAY04/src/ex04/day04_ex04.sql
@@ -0,0 +1,45 @@
+WITH R AS (
+    SELECT person_id
+    FROM person_visits
+    WHERE visit_date = '2022-01-02'
+),
+     S AS (
+         SELECT person_id
+         FROM person_visits
+         WHERE visit_date = '2022-01-06'
+     )
+SELECT person_id
+FROM (SELECT person_id FROM R EXCEPT SELECT person_id FROM S) t1
+UNION
+(SELECT person_id FROM S EXCEPT SELECT person_id FROM R)
+ORDER BY 1;
+CREATE VIEW v_symmetric_union AS
+SELECT person_id
+FROM (SELECT person_id
+      FROM (
+               SELECT person_id
+               FROM person_visits
+               WHERE visit_date = '2022-01-02'
+           ) R
+      EXCEPT
+      SELECT person_id
+      FROM (
+               SELECT person_id
+               FROM person_visits
+               WHERE visit_date = '2022-01-06'
+           ) S) t1
+UNION
+(SELECT person_id
+ FROM (
+          SELECT person_id
+          FROM person_visits
+          WHERE visit_date = '2022-01-06'
+      ) S
+ EXCEPT
+ SELECT person_id
+ FROM (
+          SELECT person_id
+          FROM person_visits
+          WHERE visit_date = '2022-01-02'
+      ) R)
+ORDER BY 1;
