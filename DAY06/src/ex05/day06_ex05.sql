99836620d2764666a9e258d12a159048841fb734
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-06 12:05:25 +0300

    DAY05 ex05 done

---
+++ DAY06/src/ex05/day06_ex05.sql
@@ -0,0 +1,6 @@
+comment on table person_discounts is 'Table with personal discounts';
+comment on column person_discounts.id is 'Identifier for every row';
+comment on column person_discounts.person_id is 'Personal identifier';
+comment on column person_discounts.pizzeria_id is 'Pizzeria identifier';
+comment on column person_discounts.discount is 'Personal discount for visitors';
+
