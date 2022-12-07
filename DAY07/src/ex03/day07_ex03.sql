1774a9744203e2159f0cb3ede32d5416a2dfebee
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-07 12:09:48 +0300

    DAY07 ex03 done

---
+++ DAY07/src/ex03/day07_ex03.sql
@@ -0,0 +1,16 @@
+WITH order_ AS (
+    SELECT p.name, count(m.pizzeria_id) count, ('order') action_type
+    FROM person_order po
+             JOIN menu m on po.menu_id = m.id
+             JOIN pizzeria p on p.id = m.pizzeria_id
+    GROUP BY p.name
+    ORDER BY action_type ASC, count DESC
+),
+     visit_ AS (
+         SELECT p2.name, count(person_visits.pizzeria_id) count, ('visit') action_type
+         FROM person_visits
+                  JOIN pizzeria p2 on p2.id = person_visits.pizzeria_id
+         GROUP BY p2.name
+         ORDER BY action_type ASC, count DESC
+     )
+SELECT order_.name, (order_.count + visit_.count) total_count FROM order_ JOIN visit_ ON order_.name = visit_.name
