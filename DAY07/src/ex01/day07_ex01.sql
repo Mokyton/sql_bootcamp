5bc91ecc75b927d517b6746350cd55be7a79c489
Chillwave Strong <chillwav@si-b1.kzn.21-school.ru>
2022-12-07 11:30:29 +0300

    DAY07 ex01 fix

--- DAY07/src/ex01/day07_ex01.sql
+++ DAY07/src/ex01/day07_ex01.sql
@@ -1,4 +1,4 @@
 SELECT p.name, count(pizzeria_id) count_of_visits
 FROM person_visits JOIN person p on p.id = person_visits.person_id
 GROUP BY p.name
-ORDER BY 2 DESC, 1 ASC;
+ORDER BY 2 DESC, 1 ASC LIMIT 4;
